aa9d7197f2dcb881d7e217bec00702af6799e469
yu2799 <ura.yu.2799@gmail.com>
2022-07-14 23:47:21 +0900

    fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,22 @@
 export const Footer = () => {
-    return <footer style={styles.footer}>
-        <p className="copylight">&copy; Uramaru Nakaniwa Koizumi Tsukada Tago</p>
-        <p className="hai">※ 画像は「<a href="https://mj-dragon.com/rule/">麻雀の雀龍.com</a>」の無料麻雀牌画を利用しています。</p>
+  return (
+    <footer style={styles.footer}>
+      <p className="copylight">&copy; Uramaru Nakaniwa Koizumi Tsukada Tago</p>
+      <p className="hai">
+        ※ 画像は「<a href="https://mj-dragon.com/rule/">麻雀の雀龍.com</a>
+        」の無料麻雀牌画を利用しています。
+      </p>
     </footer>
+  );
 };
 
 const styles = {
-    footer: {
-        width: "87%",
-        padding: '80px',
-        background: "#ddd",
-        textAlign: "center",
-        position: "absolute",
-        bottom: 0,
-    }
-}
+  footer: {
+    width: "87%",
+    padding: "80px",
+    background: "#ddd",
+    textAlign: "center",
+    position: "absolute",
+    bottom: 0,
+  },
+};
