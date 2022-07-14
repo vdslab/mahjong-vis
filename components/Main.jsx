ec60692a8157c4810d0b98dfdfdf69d182213983
yu2799 <ura.yu.2799@gmail.com>
2022-07-14 23:50:17 +0900

    add d3

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,6 @@
 import AbondonedHaiView from "./AbandonedHaiView";
 import AssessmentView from "./AssessmentView";
+import { Radviz } from "./Radviz";
 import TehaiView from "./TehaiView";
 import { Grid } from "@mui/material";
 export const Main = () => {
@@ -7,6 +8,7 @@ export const Main = () => {
     <div>
       <Grid container>
         <AbondonedHaiView />
+        <Radviz />
         <AssessmentView />
       </Grid>
 
