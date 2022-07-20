4014f95e5df80d3db9504d6b1cff6bcaac2a35a4
yu2799 <ura.yu.2799@gmail.com>
2022-07-20 22:46:24 +0900

    update

--- src/functions/defineFeature.js
+++ src/functions/defineFeature.js
@@ -9,11 +9,8 @@ export const defineFeature = (tehai) => {
   // 萬子筒子索子風三元の5種類
   const mentuList = [...Array(5)].map((_) => [...Array(3)].map((_) => []));
 
-  // 特徴量の初期化されたリスト
-  const featureList = FEATURE_LIST.reduce(
-    (obj, x) => Object.assign(obj, { [x]: 0 }),
-    {}
-  );
+  // 特徴量の初期化されたobject
+  const featureList = makeObject(FEATURE_LIST);
 
   // 手牌に何があるかのカウント
   const counter = tehai.reduce(
@@ -33,6 +30,8 @@ export const defineFeature = (tehai) => {
 
   // 面子構成を列挙
   mentuFeature(mentuList, counter);
+  const [md, pd, sd] = cntFeature(counter, featureList, 1, 1);
+  console.log(md, pd, sd);
   console.log(mentuList);
   console.log(featureList);
 
@@ -41,7 +40,7 @@ export const defineFeature = (tehai) => {
 
 // 面子構成を列挙
 const mentuFeature = (mentuList, counter) => {
-  const zi_set = new Set(["w", "z"]);
+  const ziSet = new Set(["w", "z"]);
 
   for (const [key, value] of Object.entries(counter)) {
     const type = key[0];
@@ -49,7 +48,7 @@ const mentuFeature = (mentuList, counter) => {
     const cnt = value;
 
     // 順子
-    if (!zi_set.has(type) && num !== 9) {
+    if (!ziSet.has(type) && num !== 9) {
       // 面子
       if (
         counter.hasOwnProperty(`${type}${num + 1}`) &&
@@ -86,8 +85,85 @@ const mentuFeature = (mentuList, counter) => {
   }
 };
 
-const cntFeature = (counter, featureList, roundWind, playerWind) => {};
+// 数系特徴量
+const cntFeature = (counter, featureList, roundWind, playerWind) => {
+  // 手牌を枚数*数字に圧縮するためのランレングス圧縮
+  const rleList = {
+    m: makeObject([...Array(9)].map((_, i) => i + 1)),
+    p: makeObject([...Array(9)].map((_, i) => i + 1)),
+    s: makeObject([...Array(9)].map((_, i) => i + 1)),
+  };
+  // 牌種の枚数
+  const typeCnt = {
+    m: 0,
+    p: 0,
+    s: 0,
+  };
+
+  const ziSet = new Set(["w", "z"]);
+
+  for (const [key, value] of Object.entries(counter)) {
+    const type = key[0];
+    const num = Number(key[1]);
+    const cnt = value;
+
+    if (!ziSet.has(type)) {
+      // 数牌の数カウント
+      if (num === 1 || num === 9) featureList["ichikyu_cnt"] += cnt;
+      else featureList["chunchan_cnt"] += cnt;
+      rleList[type][num] += cnt;
+      typeCnt[type] += cnt;
+    } else {
+      // 字牌対子,刻子の数カウント
+      if (cnt >= 2) {
+        const name = cnt === 2 ? "toitu" : cnt === 3 ? "kotu" : "kantu";
+        if (type === "z") {
+          featureList[`sangen_${name}`] += 1;
+          featureList["sangen_cnt"] += cnt;
+        } else {
+          if (playerWind == num || roundWind == num) {
+            featureList[`zikaze_bakaze_${name}`] += 1;
+            featureList["zikaze_bakaze_cnt"] += cnt;
+          } else {
+            featureList[`kaze_${name}`] += 1;
+            featureList["kaze_cnt"] += cnt;
+          }
+        }
+        featureList[`zi_${name}`] += 1;
+        featureList["zi_cnt"] += cnt;
+      }
+    }
+  }
+
+  // 手牌の中で最も多い色の数
+  featureList["same_color_cnt"] = Math.max(...Object.values(typeCnt));
+  // 手牌の中で1枚以上ある数牌の数
+  featureList["1-9_cnt"] = Math.max(
+    ...Object.values(rleList).map(
+      (data, _) => Object.values(data).filter((num) => num >= 1).length
+    )
+  );
+  // 三色同順
+  for (let i = 0; i < 7; ++i) {
+    const tmp = Object.values(rleList).reduce((prev, data) => {
+      return (
+        prev +
+        Object.values(data)
+          .slice(i, i + 3)
+          .filter((num) => num >= 1).length
+      );
+    }, 0);
+    featureList["sanshoku_cnt"] = Math.max(tmp, featureList["sanshoku_cnt"]);
+  }
+  return Object.values(rleList);
+};
+
+// 配列の中身をkeyとしたobjectを返す
+const makeObject = (array, init = 0) => {
+  return array.reduce((obj, x) => Object.assign(obj, { [x]: init }), {});
+};
 
 // 条件にあった配列の長さを返す
-const arrayFilterLength = (array, req) =>
-  array.filter((val) => val >= req).length;
+const arrayFilterLength = (array, req) => {
+  return array.filter((val) => val >= req).length;
+};
