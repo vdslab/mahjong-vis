a16159b7838e8b8fed2b42eb3181374fe19b9388
yu2799 <ura.yu.2799@gmail.com>
2022-07-21 23:40:03 +0900

    adjust how data is received

--- src/components/Radviz.jsx
+++ src/components/Radviz.jsx
@@ -4,6 +4,7 @@ import { useRecoilValue } from "recoil";
 import { defineFeature } from "../functions/defineFeature";
 import { tehaiState } from "./atoms";
 import { DIMENSIONS } from "../const/clusterValue";
+import { defineYaku } from "../functions/defineYaku";
 
 const calc_dist = (a, b, n) => {
   let sum = 0;
@@ -13,44 +14,37 @@ const calc_dist = (a, b, n) => {
   return Math.sqrt(sum);
 };
 
-const radviz = (data, dimensions, r) => {
-  const n = Object.keys(dimensions).length;
-  // const scales = Object.keys(dimensions).map((property) => {
+const radviz = (data, r) => {
+  const n = Object.keys(DIMENSIONS).length;
+  // const scales = Object.keys(DIMENSIONS).map((property) => {
   //   console.log(property);
   //   return d3
   //     .scaleLinear()
   //     .domain(d3.extent(data, (item) => item[property]))
   //     .range([0, 1]);
   // });
-  return data.map((item, i) => {
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    const dt = (2 * Math.PI) / n;
-    for (let j = 0; j < n; ++j) {
-      const v = calc_dist(item, dimensions[Object.keys(dimensions)[j]], n);
-      a += v * Math.cos(dt * j);
-      b += v * Math.sin(dt * j);
-      c += v;
-    }
-    a *= r / c;
-    b *= r / c;
-    const d = Math.sqrt(a * a + b * b);
-    const t = Math.atan2(b, a);
-    return { x: d * Math.cos(t), y: d * Math.sin(t) };
-  });
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  const dt = (2 * Math.PI) / n;
+  for (let j = 0; j < n; ++j) {
+    const v = calc_dist(data, Object.values(DIMENSIONS)[j], n);
+    console.log(v);
+    a += v * Math.cos(dt * j);
+    b += v * Math.sin(dt * j);
+    c += v;
+  }
+  a *= r / c;
+  b *= r / c;
+  const d = Math.sqrt(a * a + b * b);
+  const t = Math.atan2(b, a);
+  return { x: d * Math.cos(t), y: d * Math.sin(t) };
 };
 
 export const Radviz = () => {
   const tehai = useRecoilValue(tehaiState);
-  const feature = defineFeature(tehai);
-  const dimensions = DIMENSIONS;
-  const data = [
-    [
-      0, 0, 100, 0, 0, 56.66666667, 0, 48.33333333, 48.88888889, 0, 46, 0, 15,
-      0, 0, 0, 0, 42.85714286, 42.85714286,
-    ],
-  ];
+
+  // 図の大きさ
   const r = 300;
   const contentWidth = 2 * r;
   const contentHeight = 2 * r;
@@ -59,8 +53,15 @@ export const Radviz = () => {
   const height = contentHeight + margin * 2;
   const lineColor = "#444";
 
-  const points = radviz(data, dimensions, r);
-  // console.log(points);
+  // 手牌の特徴を計算
+  const feature = defineFeature(tehai);
+  // 役を推定
+  const data = defineYaku(feature, 14, 0);
+  // 点の座標
+  const point = radviz(data, r);
+  // 点の大きさ
+  const pointSize = 10;
+  console.log(data);
 
   return (
     <Card sx={{ p: 3, height: "100%" }}>
@@ -70,12 +71,12 @@ export const Radviz = () => {
       <svg viewBox={`0 0 ${width} ${height}`}>
         <g transform={`translate(${margin + r},${margin + r})`}>
           <circle r={r} fill="none" stroke={lineColor} />
-          {Object.keys(dimensions).map((property, i) => {
+          {Object.keys(DIMENSIONS).map((property, i) => {
             return (
               <g
                 key={i}
                 transform={`rotate(${
-                  (360 / Object.keys(dimensions).length) * i + 90
+                  (360 / Object.keys(DIMENSIONS).length) * i + 90
                 })`}
               >
                 <line
@@ -90,20 +91,20 @@ export const Radviz = () => {
                   y={-r}
                   textAnchor="middle"
                   dominantBaseline="text-after-edge"
+                  fontSize={20}
                 >
                   {property}
                 </text>
               </g>
             );
           })}
-          {data.map((item, i) => {
-            const { x, y } = points[i];
-            return (
-              <g key={i} transform={`translate(${x},${y})`}>
-                <circle r="3" opacity="0.8" />
-              </g>
-            );
-          })}
+          {point.x && point.y ? (
+            <g transform={`translate(${point.x},${point.y})`}>
+              <circle r={pointSize} opacity="0.8" />
+            </g>
+          ) : (
+            <></>
+          )}
         </g>
       </svg>
     </Card>
