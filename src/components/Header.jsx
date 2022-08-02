1e8bd632823545848bfe3fdf2e712edf0f8cfb07
yu2799 <ura.yu.2799@gmail.com>
2022-08-02 22:06:56 +0900

    (WIP) add help

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,44 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            麻雀機
+            mahjong-vis
           </Typography>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => setOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+        <List>
+          {["a", "b", "help"].map((text, idx) => {
+            return <ListItem key={idx}>{text}</ListItem>;
+          })}
+        </List>
+      </Drawer>
     </Box>
   );
 };
