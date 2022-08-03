2c263f3c93bb8ba3c33b8524881e48624e384082
unibon4141 <vhcdyufg@gmail.com>
2022-08-03 17:36:01 +0900

    ツモボタンを押下した際に役名が表示されるように変更

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,15 +1,44 @@
 import { Dialog, DialogTitle, Stack, Typography } from "@mui/material";
+import { tehaiState } from "./atoms";
+import { defineFeature } from "../functions/defineFeature";
+import { defineYaku } from "../functions/defineYaku";
+import { useRecoilValue } from "recoil";
 
 export const DisplayInfo = (props) => {
   const { onClose, open } = props;
   const data = [1, 2, 3];
-
+  const { featureList, shanten } = defineFeature(useRecoilValue(tehaiState));
+  const data1 = defineYaku(featureList, 14, 0);
+  const YakuNameDisplaied = {
+    chanta: "混全帯么九",
+    chinitu: "清一色",
+    chitoitu: "七対子",
+    honitu: "混一色",
+    honroto: "混老頭",
+    ipeko: "一盃口",
+    ittu: "一気通貫",
+    junchan: "純全帯么九",
+    pinfu: "平和",
+    ryanpeko: "二盃口",
+    sananko: "三暗刻",
+    sangenhai: "三元牌",
+    sankantu: "三槓子",
+    sanshoku_dojun: "三色同順",
+    sanshoku_douko: "三色同刻",
+    shosangen: "小三元",
+    shousangen: "小三元",
+    tanyao: "断么九",
+    toitoiho: "対々和",
+    zikaze_bakaze: "字牌,場風",
+  };
   return (
     <Dialog onClose={onClose} open={open}>
       <DialogTitle>結果</DialogTitle>
       <Stack sx={{ p: 1 }}>
-        {data.map((i, idx) => {
-          return <Typography key={idx}>{i}</Typography>;
+        {Object.keys(data1).map((key, idx) => {
+          if (data1[key] === 100) {
+            return <Typography key={idx}>{YakuNameDisplaied[key]}</Typography>;
+          }
         })}
       </Stack>
     </Dialog>
