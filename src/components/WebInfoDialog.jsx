499650cf255fe08c2955b08a279a51e4e25009b6
yu2799 <ura.yu.2799@gmail.com>
2022-11-07 15:13:31 +0900

    fix style

--- src/components/WebInfoDialog.jsx
+++ src/components/WebInfoDialog.jsx
@@ -1,10 +1,8 @@
-import { memo } from "react";
+import { Fragment, memo } from "react";
 import Image from "next/image";
 import Images from "../../public/images/webImage/webImage.png";
 import {
   Box,
-  Card,
-  CardContent,
   Dialog,
   DialogContent,
   DialogTitle,
@@ -26,7 +24,7 @@ export const WebInfoDialog = memo((props) => {
       <DialogTitle id="alert-dialog-title">
         <Box sx={{ display: "flex" }}>
           <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
-            役説明
+            このサイトについて
           </Typography>
           <IconButton onClick={onClose}>
             <CloseIcon />
@@ -41,56 +39,62 @@ export const WebInfoDialog = memo((props) => {
 });
 
 const Contents = () => {
+  const text = [
+    ["1.河", "手牌から捨てた牌を表示します。"],
+    [
+      "2.Radviz",
+      "手牌が今どの役に近いかを示しています。",
+      "赤い点→現在の手牌14枚から考えられる点",
+      "緑の点→対応する牌を捨てた後に、どこに移動するかを予測した点",
+    ],
+    [
+      "3.ヒートマップ",
+      "それぞれの牌を捨てた際にどのくらい左側の役に近づけるかをそれぞれ表示しています。",
+      "青色に近いほど役から遠ざかり、赤色に近いほど役に近づいていきます。",
+    ],
+    [
+      "4.手牌",
+      "現在持っている手牌を表示します。カーソルを合わせることで、Radvizにおける緑点の強調表示や、評価値表における牌の強調表示を行えます。",
+      "青矢印→捨てることで、向聴数（リーチまであと何手手前か）が増えることを示しています。",
+      "黄緑矢印→捨てても向聴数が変化しません。",
+      "赤矢印→捨てることで、向聴数が減ることを示しています。",
+      "リセット→手牌をリセットします。",
+      "ツモ（アガれる場合のみ）→揃った役を確認後、手牌をリセットします。",
+    ],
+    [
+      "5.ツモモード",
+      "出現させる牌の種類を選択することができます。",
+      "・すべての牌:全種類の牌が出現",
+      "・混一色:萬子と字牌のみが出現",
+      "・清一色:萬子のみが出現",
+      "・国士無双:19牌と字牌のみが出現",
+    ],
+  ];
+
   return (
-    <>
-      <Typography variant="h6">
-        ①河
-        <br />
-        手牌から捨てた牌を表示します。
-        <br />
-        <br />
-        ➁Radviz
-        <br />
-        手牌が今どの役に近いかを示しています。
-        <br />
-        赤い点→現在の手牌14枚から考えられる点
-        <br />
-        緑の点→対応する牌を捨てた後に、どこに移動するかを予測した点
-        <br />
-        <br />
-        ③評価値表
-        <br />
-        それぞれの牌を捨てた際にどのくらい左側の役に近づけるかをそれぞれ表示しています。
-        <br />
-        青色に近いほど役から遠ざかり、赤色に近いほど役に近づいていきます。
-        <br />
-        <br />
-        ④手牌
-        <br />
-        現在持っている手牌を表示します。カーソルを合わせることで、Radvizにおける緑点の
-        強調表示や、評価値表における牌の強調表示を行えます。
-        <br />
-        青い矢印→捨てることで、向聴数（リーチまであと何手手前か）が増えることを示しています。
-        <br />
-        緑の矢印→捨てても向聴数が変化しません。
-        <br />
-        赤い矢印→捨てることで、向聴数が減ることを示しています。
-        <br />
-        自摸モード→出現させる牌の種類を選択することができます。
-        <br />
-        ・すべての牌：全種類の牌が出現
-        <br />
-        ・混一色：1種類の牌と字牌のみが出現
-        <br />
-        ・清一色：1種類の牌のみが出現
-        <br />
-        ・国士無双：1,9牌と字牌のみが出現
-        <br />
-        リセット→手牌をリセットします。
-        <br />
-        ツモ（上がれる場合のみ）→揃った役を確認後、手牌をリセットします。
-      </Typography>
+    <Box>
+      {text.map((tmp) => {
+        const title = tmp[0];
+        const buf = tmp.slice(1);
+        return (
+          <Fragment key={title}>
+            <Typography variant="h6" component="div">
+              {title}
+            </Typography>
+            {buf.map((description, i) => (
+              <Typography
+                key={`${buf}${i}`}
+                gutterBottom
+                variant="body1"
+                color="text.secondary"
+              >
+                {description}
+              </Typography>
+            ))}
+          </Fragment>
+        );
+      })}
       <Image src={Images} alt="Image" />
-    </>
+    </Box>
   );
 };
