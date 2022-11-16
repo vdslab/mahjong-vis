0aa7249861e8a8a8cf600dd55a043eadcd38126b
yu2799 <ura.yu.2799@gmail.com>
2022-11-16 13:38:21 +0900

    change the order of dimensions

---
+++ src/const/dimensions.js
@@ -0,0 +1,18 @@
+export const DIMENSIONS = [
+  "tanyao",
+  "pinfu",
+  "ittu",
+  "honitu",
+  "chinitu",
+  "junchan",
+  "chanta",
+  "sangenhai",
+  // "honroto",
+  "shosangen",
+  "sananko",
+  "zikaze_bakaze",
+  "ipeko",
+  // "ryanpeko",
+  "chitoitu",
+  "sanshoku_dojun",
+];
