52495f617a5c6cfb9fdbb5606effc20a2aa2037a
yu2799 <ura.yu.2799@gmail.com>
2022-11-26 17:02:58 +0900

    add seo

---
+++ next-seo.config.js
@@ -0,0 +1,28 @@
+export const SEO = {
+  defaultTitle: "mahjong-vis",
+  description: "mahjong-visは、手牌構造に注目した特徴可視化システムです",
+  canonical: "https://mahjong-vis.vdslab.jp/",
+  additionalMetaTags: [
+    {
+      property: "dc:creator",
+      content: "team2022-namako",
+    },
+    {
+      name: "application-name",
+      content: "mahjong-vis",
+    },
+  ],
+  openGraph: {
+    url: "https://mahjong-vis.vdslab.jp/",
+    type: "website",
+    locale: "ja_JP",
+    site_name: "mahjong-vis",
+    // images: [
+    //   {
+    //     url: "./images/sample.png",
+    //     width: 800,
+    //     height: 600,
+    //   },
+    // ],
+  },
+};
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,14 @@
 import { CssBaseline } from "@mui/material";
 import { RecoilRoot } from "recoil";
+import { DefaultSeo } from "next-seo";
+import { SEO } from "../../next-seo.config";
 
 const MyApp = (props) => {
   const { Component, pageProps } = props;
 
   return (
     <RecoilRoot>
+      <DefaultSeo {...SEO} />
       <CssBaseline>
         <Component style={{ margin: 0 }} {...pageProps} />
       </CssBaseline>
